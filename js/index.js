aac29e66850a23643d489dd911dfa421467e3922
Jess <jess.southey@live.com>
2021-07-22 21:16:19 +1000

    Week 19

--- js/index.js
+++ js/index.js
@@ -6,18 +6,18 @@ $("html, body").animate({scrollTop: 0}, "slow");
 
 });
 
-(function($){
-    'use strict';
-      $(window).on('load', function () {
-          if ($(".pre-loader").length > 0)
-          {
-              $(".pre-loader").fadeOut("slow");
-          }
-    setTimeout(fade, 3000);
-      });
-  })(jQuery)
+// (function($){
+//     'use strict';
+//       $(window).on('load', function () {
+//           if ($(".pre-loader").length > 0)
+//           {
+//               $(".pre-loader").fadeOut("slow");
+//           }
+//     setTimeout(fade, 3000);
+//       });
+//   })(jQuery)
 
-  function fade() {
-    $('.pre-loader').fadeOut("slow");
-    }
-    setTimeout(fade, 3000);
+//   function fade() {
+//     $('.pre-loader').fadeOut("slow");
+//     }
+//     setTimeout(fade, 3000);
