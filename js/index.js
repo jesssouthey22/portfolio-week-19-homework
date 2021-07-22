85e0d8d65ac90874e1f81ec32e876d19efaee7b9
Jess <jess.southey@live.com>
2021-07-22 21:06:22 +1000

    Week 19 Homework

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const btnScrollToTop = document.querySelector("btnScrollToTop")
+
+$("#btnScrollToTop").addEventListener("click", function() {
+
+$("html, body").animate({scrollTop: 0}, "slow");
+
+});
+
+(function($){
+    'use strict';
+      $(window).on('load', function () {
+          if ($(".pre-loader").length > 0)
+          {
+              $(".pre-loader").fadeOut("slow");
+          }
+    setTimeout(fade, 3000);
+      });
+  })(jQuery)
+
+  function fade() {
+    $('.pre-loader').fadeOut("slow");
+    }
+    setTimeout(fade, 3000);
