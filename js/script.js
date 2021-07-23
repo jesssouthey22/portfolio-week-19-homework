fc0d0a5595bf97aee267e9a362b6e5ebf4479045
Jess <jess.southey@live.com>
2021-07-23 17:07:05 +1000

    Week 19

---
+++ js/script.js
@@ -0,0 +1,46 @@
+
+
+// $("#btnScrollToTop").addEventListener("click", function() {
+
+// $("html, body").animate({scrollTop: 0}, "slow");
+
+// });
+
+
+
+
+$(document).ready(function(){
+
+    
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 100) {
+          $('#btnScrollToTop').fadeIn();
+      } else {
+          $('#btnScrollToTop').fadeOut();
+      }
+  });
+
+  
+     $('#btnScrollToTop').click(function(){
+      $('html, body').animate({scrollTop : 0},800);
+      return false;
+     });
+
+
+    $('#workNav').click(function(){
+        $([document.documentElement, document.body]).animate({
+          scrollTop: $(".casestudyGrid").offset().top
+        }, 2000);
+
+    });
+});
+
+
+
+    //   var div = $(this);
+    //   div.animate({height: '300px', opacity: '0.4'}, "slow");
+    //   div.animate({width: '300px', opacity: '0.8'}, "slow");
+    //   div.animate({height: '100px', opacity: '0.4'}, "slow");
+    //   div.animate({width: '100px', opacity: '0.8'}, "slow");
+    // });
+
