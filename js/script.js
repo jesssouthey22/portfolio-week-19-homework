ea890d98b31bbf66ceeed5d2af461adcab3ee73e
Jess <jess.southey@live.com>
2021-07-24 14:16:19 +1000

    Week 19

--- js/script.js
+++ js/script.js
@@ -23,14 +23,14 @@ $(document).ready(function(){
 
     });
 
-    window.onscroll = function() {myFunction()};
+    // window.onscroll = function() {myFunction()};
 
-    function myFunction() {
-      var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-      var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-      var scrolled = (winScroll / height) * 100;
-      document.getElementById("myProgressBar").style.width = scrolled + "%";
-      document.getElementById("myProgressBar").innerHTML= Math.round(scrolled) + "%";
+    // function myFunction() {
+    //   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    //   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    //   var scrolled = (winScroll / height) * 100;
+    //   document.getElementById("myProgressBar").style.width = scrolled + "%";
+    //   document.getElementById("myProgressBar").innerHTML= Math.round(scrolled) + "%";
     }
 
 
